95ac929747ba7b896f65583390f069f3206d882c
joseunghui <seung00097@gmail.com>
2023-10-26 22:04:04 +0900

    Initial commit

---
+++ src/test/java/com/potatoes/BloodRecoverybatch/BloodRecoveryBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.potatoes.BloodRecoverybatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodRecoveryBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
