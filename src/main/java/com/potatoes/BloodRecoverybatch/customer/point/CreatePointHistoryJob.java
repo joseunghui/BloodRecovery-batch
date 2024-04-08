435073487d312bce21b42ca3679bcec5df614e41
Seung <seung00097@gmail.com>
2024-04-08 12:27:54 +0900

    배치 api - 포인트

--- src/main/java/com/potatoes/BloodRecoverybatch/customer/point/CreatePointHistoryJob.java
+++ src/main/java/com/potatoes/BloodRecoverybatch/customer/point/CreatePointHistoryJob.java
@@ -1,4 +1,9 @@
 package com.potatoes.BloodRecoverybatch.customer.point;
 
 public class CreatePointHistoryJob {
+    // point batch
+    /**
+     * plus(minus) point
+     * search(get total) point
+     */
 }
